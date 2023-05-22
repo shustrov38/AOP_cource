d2fc30f83b33dd7317df7e43120af3ce686c6122
akon1te <anros_t@mail.ru>
2023-05-22 22:05:38 +0300

    knapsack ver.2

--- lab_3/algorithms/knapsack_genetic.py
+++ lab_3/algorithms/knapsack_genetic.py
@@ -2,43 +2,78 @@ import random
 from typing import List, Tuple
 import numpy as np
 
+MAGIC_NUMBER = 54
 
-def fitness_function(chrom: List[int], capacity: int, items_weights: List[int], items_cost: List[int]) -> Tuple[int, int]:
+
+def crossover_and_mutation_function(chroms: List[int], items_size: int, k: int) -> List[int]:
     
-    result_weights = chrom * items_weights
-    result_cost = chrom * items_cost
+    crossover = random.randint(1, items_size - 1) // 2
+    crossover *= 2
+
+    for j in range(0, k, 2):
+        for i in range(crossover):
+            chroms[j][i], chroms[j + 1][i] = chroms[j + 1][i], chroms[j][i]
+
+    for j in range(items_size):
+        for i in range(len(chroms[j])):
+            if random.randint(1, 100) == MAGIC_NUMBER:
+                chroms[j][i] = (chroms[j][i] + 1) % 2
+
+    return chroms
+
+
+def fitness_function(chroms: List[int], capacity: int, items_weights: List[int], items_cost: List[int]) -> Tuple[int]:
+
+    result_weights = chroms * items_weights
+    result_cost = chroms * items_cost
 
     while result_weights > capacity:
-        nonzero_chrom = np.nonzero(chrom)[0]
-        chrom[nonzero_chrom[random.randint(0, len(nonzero_chrom) - 1)]] = 0
-        
-        result_weights = chrom * items_weights
-        result_cost = chrom * items_cost
-    
+        nonzero_chrom = np.nonzero(chroms)[0]
+        chroms[nonzero_chrom[random.randint(0, len(nonzero_chrom) - 1)]] = 0
+
+        result_weights = chroms * items_weights
+        result_cost = chroms * items_cost
+
     return result_cost
 
 
 def evolve(capacity: int, items_weights: List[int], items_cost: List[int], ):
-    
+
     items_weights, items_cost = np.array(items_weights), np.array(items_cost)
     items_size = len(items_cost)
-    
+
     chroms_size = 100
-    chroms = np.array([[random.randint(0, 1) for x in range(items_size)] for _ in range(chroms_size)])
-    
+    chroms = np.array([[random.randint(0, 1)
+                      for _ in range(items_size)] for _ in range(chroms_size)])
+
     k = chroms_size // 4
     k = k // 2
     k *= 2
-    
+
     fitness_results = []
-    for chrom in range(chroms_size):
-        fitness_results.append((chrom, fitness_function(chroms[chrom], capacity, 
-                                                items_weights, items_cost)))
-        
+    for chroms in range(chroms_size):
+        fitness_results.append((chroms, fitness_function(chroms[chroms], capacity,
+                                                         items_weights, items_cost)))
+
     fitness_results.sort(key=lambda x: x[1], reverse=True)
     fitness_sum = 0
     for x in fitness_results:
         fitness_sum += x[1]
-        
+
     p = [x[0] / fitness_sum for x in fitness_results]
     selected_chroms = random.choices(chroms, weights=p, k=k)
+
+    selected_chroms = crossover_and_mutation_function(
+        selected_chroms, items_size, k)
+
+    new_fitness_results = []
+    for chroms in range(len(selected_chroms)):
+        new_fitness_results.append((chroms, fitness_function(selected_chroms[chroms], capacity,
+                                                             items_weights, items_cost)))
+
+    new_fitness_results.sort(key=lambda x: x[1], reverse=True)
+
+    if new_fitness_results[0][1] > fitness_results[0][1]:
+        return selected_chroms[new_fitness_results[0][0]], new_fitness_results[0][1]
+    else:
+        return chroms[fitness_results[0][0]], fitness_results[0][1]
